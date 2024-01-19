d9f2e515b62584860eaaf1338f30954024e5fdd5
krzemLech <krzemmo@gmail.com>
2024-01-19 22:39:22 +0100

    added logging

--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const { validateMsg } = require("./validation/validation");
 const { connectDB } = require("./db/db");
 const { messageController } = require("./controller/messages");
+var morgan = require("morgan");
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
   res.json({ status: "ok" });
