587dbab6f7e5708a514136a4ace7813413621b85
krzemLech <krzemmo@gmail.com>
2024-01-20 15:39:51 +0100

    debuging 1

--- src/auth/authMiddleware.js
+++ src/auth/authMiddleware.js
@@ -2,7 +2,8 @@ const { APP_ID } = require("../config");
 
 exports.checkAppId = (req, res, next) => {
   const appId = req.headers["x-app-id"];
-  if (appId !== APP_ID) {
+  console.log(req.headers);
+  if (appId.trim() !== APP_ID) {
     return res.status(401).json({ msg: "Unauthorized" });
   }
   next();
